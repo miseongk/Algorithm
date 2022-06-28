09262efd7634385145f8fff84210c0f66a3a5bdc
Miseong Kim <miseongk@naver.com>
2022-06-28 21:12:14 +0900

    add comment

--- Dijkstra/dijkstra_example2.py
+++ Dijkstra/dijkstra_example2.py
@@ -22,14 +22,18 @@ for _ in range(m):
 
 def dijkstra(start):
     q = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여, 큐에 삽입
     heapq.heappush(q, (0, start))
     distance[start] = 0
-    while q:
+    while q:  # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
             continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now]:
             cost = dist + i[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
