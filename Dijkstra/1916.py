89f2df734d97be267707f3c29cfab3340ceb80b2
Miseong Kim <miseongk@naver.com>
2022-06-29 09:52:12 +0900

    #1916

---
+++ Dijkstra/1916.py
@@ -0,0 +1,32 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+start, end = map(int, input().split())
+
+q = []
+distance[start] = 0
+heapq.heappush(q, (0, start))
+
+while q:
+    dist, now = heapq.heappop(q)
+    if dist > distance[now]:
+        continue
+    for i in graph[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost
+            heapq.heappush(q, (cost, i[0]))
+
+print(distance[end])
