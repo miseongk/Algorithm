a036d3351dfacbe7f5766b19e4a00f18cc90332a
Miseong Kim <miseongk@naver.com>
2022-08-04 17:01:51 +0900

    [#14719] add example

--- BAEKJOON/Part/Part2/14719.py
+++ BAEKJOON/Part/Part2/14719.py
@@ -23,3 +23,23 @@ for i in range(h-1, -1, -1):
             left, right = right, -1
 
 print(cnt)
+
+# [참고]
+# H, W = map(int, input().split())
+# A = list(map(int, input().split()))
+# M = []
+# m = -1
+# for i in range(len(A)-1, -1, -1):
+#     if m < A[i]:
+#         m = A[i]
+#     M = [m] + M
+#
+# R = 0
+# m = A[0]
+# for i in range(1, len(A)-1):
+#     t = min(m, M[i]) - A[i]
+#     if t > 0:
+#         R += t
+#     if m < A[i]:
+#         m = A[i]
+# print(R)
